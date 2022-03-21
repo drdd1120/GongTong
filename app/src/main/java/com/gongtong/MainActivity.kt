278ffd945989c18d345da0cbfb759318e87e7717
BlancCarte <yusung0924@naver.com>
2022-03-21 16:03:36 +0900

    테스트3

--- app/src/main/java/com/gongtong/MainActivity.kt
+++ app/src/main/java/com/gongtong/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 // test
 // 유성 테스트2
 // 재헌 테스트2
+// 유성 테스트3
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
