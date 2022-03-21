72098db0baf84d5a35860a103719851f97970b8c
BlancCarte <yusung0924@naver.com>
2022-03-21 15:59:11 +0900

    jys

--- app/src/main/java/com/gongtong/MainActivity.kt
+++ app/src/main/java/com/gongtong/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 // 처음
 // test
+// 유성 테스트2
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
