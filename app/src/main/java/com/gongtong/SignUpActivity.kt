ba3f637929e562a5341edde8e7a432c4a2e6d26e
Jaeheon Jeong <drdd1120@naver.com>
2022-04-01 20:33:06 +0900

    이미지

--- app/src/main/java/com/gongtong/SignUpActivity.kt
+++ app/src/main/java/com/gongtong/SignUpActivity.kt
@@ -3,6 +3,12 @@ package com.gongtong
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// shared 조사 적용
+// sign up 코드 구현,
+// 의사소통판 ui(약구현)
+// 네이버 클라우드 플랫폼 연동
+
+
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
