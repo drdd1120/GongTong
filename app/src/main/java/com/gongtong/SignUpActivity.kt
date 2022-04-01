095c205adbaf06afc90c75633bb0e8e717bfd8bd
Jaeheon Jeong <drdd1120@naver.com>
2022-04-01 19:59:37 +0900

    회원등록 xml

--- app/src/main/java/com/gongtong/SignUpActivity.kt
+++ app/src/main/java/com/gongtong/SignUpActivity.kt
@@ -1,27 +1,11 @@
 package com.gongtong
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
-import com.gongtong.databinding.ActivitySignUpBinding
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
-
-private var firebaseFirestore: FirebaseFirestore? = null
 
 class SignUpActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySignUpBinding
-
-    val db = Firebase.firestore //파이어베이스.파이어스토어 설정
-
     override fun onCreate(savedInstanceState: Bundle?) {
-
         super.onCreate(savedInstanceState)
-
-        binding = ActivitySignUpBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        setContentView(R.layout.activity_sign_up)
     }
 }
