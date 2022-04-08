f3592cfa04f4f2aba4b4befc2caa558454d1ac76
gyuho0703 <gyuho0789@naver.com>
2022-04-09 01:54:33 +0900

    음성 설정

--- app/src/main/java/com/gongtong/settings/VoiceSettingActivity.kt
+++ app/src/main/java/com/gongtong/settings/VoiceSettingActivity.kt
@@ -2,6 +2,8 @@ package com.gongtong.settings
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.gongtong.R
 import com.gongtong.databinding.ActivityMainBinding
 import com.gongtong.databinding.ActivitySignUpBinding
 import com.gongtong.databinding.ActivityVoiceSettingBinding
@@ -11,6 +13,7 @@ class VoiceSettingActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityVoiceSettingBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
 
     }
