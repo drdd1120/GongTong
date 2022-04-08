074235fd68267061b01d0d2155ad9db1ff9d0098
gyuho0703 <gyuho0789@naver.com>
2022-04-08 22:00:23 +0900

    음성 선택 화면

---
+++ app/src/main/java/com/gongtong/settings/VoiceSettingActivity.kt
@@ -0,0 +1,11 @@
+package com.gongtong.settings
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class VoiceSettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_voice_setting)
+    }
+}
