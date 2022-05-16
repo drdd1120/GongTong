789371ee672c0772bf64b831996be1baee447b5c
him0814 <him1325@gmail.com>
2022-05-16 14:30:11 +0900

    재익 커밋

--- app/src/main/java/com/gongtong/GridAdapter.kt
+++ app/src/main/java/com/gongtong/GridAdapter.kt
@@ -1,62 +1,27 @@
 package com.gongtong
 
-import android.annotation.SuppressLint
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
-import java.text.SimpleDateFormat
+import com.gongtong.databinding.ActivityHomeBinding
+import com.gongtong.databinding.ActivityHomeBinding.inflate
 import com.gongtong.databinding.ItemRecyclerBinding
+import java.text.SimpleDateFormat
 import java.util.*
 
-class GridAdapter(
-    val onItemClicked: (GridData) -> Unit) : ListAdapter<GridData, GridAdapter.ViewHolder>(diffUtil) {
+class GridAdapter(val binding: MutableList<GridData>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    inner class ViewHolder(private val binding: ItemRecyclerBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+    var listData = mutableListOf<GridData>()
 
-        fun bind(griddata: GridData) {
-            binding.name.text = griddata.name
-            Log.d("이미지 주소",griddata.image_url )
-            // glide로 이미지 불러오기;
-            if (griddata.image_url.isNotEmpty()) {
-                Glide.with(binding.thumbnailImageView)
-                    .load(griddata.image_url)
-                    .into(binding.thumbnailImageView)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        GridViewHolder(ItemRecyclerBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+
+    override fun getItemCount(): Int = datas.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding = (holder as GridViewHolder).binding
+        binding.itemData.text = datas[position]
             }
-            binding.root.setOnClickListener {
-                onItemClicked(griddata)
-            }
-        }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(
-            ItemRecyclerBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
-        )
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(currentList[position])
-    }
-
-    companion object {
-        val diffUtil = object : DiffUtil.ItemCallback<GridData>() {
-            override fun areItemsTheSame(oldItem: GridData, newItem: GridData): Boolean {
-                // 현재 노출하고 있는 아이템과 새로운 아이템이 같은지 비교;
-                return oldItem.image_url == newItem.image_url
-            }
-            override fun areContentsTheSame(oldItem: GridData, newItem: GridData): Boolean {
-                // equals 비교;
-                return oldItem == newItem
-            }
-        }
-    }
 }
+
+class GridViewHolder(val binding: ItemRecyclerBinding): RecyclerView.ViewHolder(binding.root)
